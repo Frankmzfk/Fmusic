4c6b44bcf40b7bc0528a888e2372febf439150f9
Frankmzfk <busesarefrank@gmail.com>
2020-12-14 19:01:08 +0700

    Автоизмения студии и фундамент для дальнейшей разработки

---
+++ app/src/main/java/com/example/fmusic/Data.java
@@ -0,0 +1,13 @@
+package com.example.fmusic;
+
+import android.widget.Adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Data
+{
+    private static List<Music> my_music;
+    private static List<Music> all_music;
+}
---
+++ app/src/main/java/com/example/fmusic/DetailMusic.java
@@ -0,0 +1,14 @@
+package com.example.fmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_music);
+    }
+}
