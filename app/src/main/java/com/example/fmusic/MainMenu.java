98891bc05522d776943cb3ce515c8b7cbea8d7c5
Frankmzfk <busesarefrank@gmail.com>
2020-12-14 18:59:05 +0700

    Связал кнопки

--- app/src/main/java/com/example/fmusic/MainMenu.java
+++ app/src/main/java/com/example/fmusic/MainMenu.java
@@ -3,12 +3,19 @@ package com.example.fmusic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button go_music;
+    Button go_fav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        go_music = (Button) findViewById(R.id.go_music);
+        go_fav = (Button) findViewById(R.id.go_favorites);
     }
 }
